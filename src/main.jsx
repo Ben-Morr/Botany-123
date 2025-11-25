9363ecc0d0a7f1c86c87ff3277d4a5fd080d3bc2
Ben <benmorr98@gmail.com>
2025-11-25 14:40:46 -0600

    built

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/botany-123">
       <App />
     </BrowserRouter>
   </StrictMode>,
